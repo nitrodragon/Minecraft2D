a5c9e05e13e3242aaeadf2ca876caedb9894bd4e
Reagan Russell <nitrodragon@users.noreply.github.com>
2017-07-08 11:39:54 -0700

    Add files via upload

---
+++ src/BlockType.java
@@ -0,0 +1,11 @@
+package minecraft2d;
+
+public enum BlockType {
+	STONE("res/stone.png"), AIR("res/air.png"), GRASS("res/grass.png"), DIRT("res/dirt.png"), 
+	WOOD("res/wood2.png"), BRICK("res/brick.png"), WATER("res/water.png"), LAVA("res/lava.png"),
+	LEAF("res/leaf.png"), SQUIDWOOD("res/slime.png");
+	public final String location;
+	BlockType(String location) {
+		this.location = location;
+	}
+}
