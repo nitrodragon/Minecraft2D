a1349a722130db300a3fef7ad2366abd926738f6
Reagan Russell <nitrodragon@users.noreply.github.com>
2017-07-08 12:59:12 -0700

    Update BlockType.java

--- src/minecraft2d/BlockType.java
+++ src/minecraft2d/BlockType.java
@@ -5,6 +5,7 @@ public enum BlockType {
 	WOOD("res/wood2.png"), BRICK("res/brick.png"), WATER("res/water.png"), LAVA("res/lava.png"),
 	LEAF("res/leaf.png"), SQUIDWOOD("res/slime.png");
 	public final String location;
+	// location as in file location
 	BlockType(String location) {
 		this.location = location;
 	}
